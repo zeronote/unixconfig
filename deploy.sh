36852d38ad51285835fddd706d683b53ab8c71dd
Lorenzo Chianura <lorenzo.chianura@audinate.com>
2023-07-20 11:16:45 +1000

    Add tmux config, update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-if [[ -f ~/.vimrc ]]; then
-    echo "backup current vimrc to ~/vimrc_back"
-    cp ~/.vimrc ~/vimrc_back
-fi
+# uncomment for testing
+#HOME="/tmp"
 
-if [[ -f ~/.bashrc ]]; then
-    echo "backup current bashrc to ~/bashrc_back"    
-    cp ~/.bashrc ~/bashrc_back
-fi
+backup() {
+    filepath="$HOME/$1"
+    now=$(date +"%Y%m%d_%H%M%S")
+    if [[ -f "${filepath}" ]]; then
+        backupfile="${filepath}"_back_"${now}"
+        cp "${filepath}" "${backupfile}"
+        echo "existing ${filepath} saved to ${backupfile}"
+    fi
+}
 
-cp vimrc ~/.vimrc
-cp bashrc ~/.bashrc
+copy_hidden_conf() {
+    filename="$1"
+    cp "${filename}" $HOME/."${filename}"
+    echo "${filename}" copied to $HOME/."${filename}"
+}
+
+backup ".vimrc"
+backup ".bashrc"
+backup ".tmux.conf"
+
+copy_hidden_conf "vimrc"
+copy_hidden_conf "bashrc"
+copy_hidden_conf "tmux.conf"
